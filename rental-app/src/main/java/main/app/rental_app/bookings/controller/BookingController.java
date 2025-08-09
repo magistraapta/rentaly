1f5a7064f2ad88e3eded10e445ab3d626eda2eb0
Magistra Apta <31767116+magistraapta@users.noreply.github.com>
2025-08-09 14:37:53 +0800

    fix: add authority filter

--- rental-app/src/main/java/main/app/rental_app/bookings/controller/BookingController.java
+++ rental-app/src/main/java/main/app/rental_app/bookings/controller/BookingController.java
@@ -2,6 +2,7 @@ package main.app.rental_app.bookings.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -45,6 +46,7 @@ public class BookingController {
     }
 
     @GetMapping("/all")
+    @PreAuthorize("hasAuthority('admin')")
     public ResponseEntity<BaseResponse<List<Invoices>>> getAllInvoices() {
         try {
             log.info("Controller: Getting all invoices");
